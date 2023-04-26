6bb4219bb6ebe07420fed5bae3a2feba8cfed430
Faith Mukungurutse <faithepy@gmail.com>
2023-04-26 11:38:30 -0700

    Putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,74 +1,19 @@
-Task 0
-
-
-#include <stdio.h>
-
-#include <stdlib.h>
-
-#include <time.h>
-
-/**
-
- * main - Entry point
-
- *
-
- * Return: Always 0 (success)
-
- */
-
-
-int main(void)
-
-{
-
-
-printf("_putchar\n");
-
-
-return (0);
-
-}
-
-
-
-
-Task 1
-
-
 #include "main.h"
-
-#include <unistd.h>
-
-
 /**
-
- * print_alphabet - Entry point
-
- * Return:0
-
+ * main - prints robert to stdout
+ *
+ * Return: 0 if no error
  */
-
-
-void print_alphabet(void)
-
+int main(void)
 {
-
-        char c = 'a';
-
-
-        while (c <= 'z')
-
-        {
-
-                _putchar(c);
-
-                c++;
-
-
-        }
-
-        _putchar('\n');
-
-
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+	return (0);
 }
