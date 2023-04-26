b4669f2bd9b6b49c99dcb6c1f72786671a2ceec3
Faith Mukungurutse <faithepy@gmail.com>
2023-04-26 11:59:46 -0700

    Putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,9 @@
 #include "main.h"
 
 /**
-*main - print _putchar and a new line
-*
-*Return: Always 0
-*/
+ * main - Print _putchar
+ * Return: Always 0
+ */
 int main(void)
 {
 	_putchar('_');
@@ -16,6 +15,5 @@ int main(void)
 	_putchar('a');
 	_putchar('r');
 	_putchar('\n');
-
 	return (0);
 }
